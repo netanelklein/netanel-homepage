3babdfb73d0ef1c801c0f2103fefb9ff58035351
netanelklein <netanel@netanelk.com>
2025-05-23 14:58:36 +0300

    Removed unused imports

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:netanel_homepage/presentation/pages/home_page.dart';
-import 'package:netanel_homepage/main.dart'; // Adjust if your main app file is elsewhere
+//import 'package:netanel_homepage/presentation/pages/home_page.dart';
+//import 'package:netanel_homepage/main.dart'; // Adjust if your main app file is elsewhere
 
 void main() {
   test('Home page loads and displays', () {
